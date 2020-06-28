5267817a44d4a2cb679a37c9dca4c3e6a2caab6c
YimingPro <39811915+YimingPro@users.noreply.github.com>
2020-06-29 02:41:59 +0800

    Add files via upload

---
+++ Drone_motors.ino
@@ -0,0 +1,96 @@
+// demo: CAN-BUS Shield, receive data with interrupt mode, and set mask and filter
+//
+// when in interrupt mode, the data coming can't be too fast, must >20ms, or else you can use check mode
+// loovee, 2014-7-8
+
+#include <SPI.h>
+#include "mcp_can.h"
+#include <Wire.h>
+#include "Arduino.h"
+#include "AX12A.h"
+#define DirectionPin   (10u)
+#define BaudRate      (1000000ul)
+#define ID        (1u)
+
+//  VNH2SP30 pin definitions// if we use DC motor, this is the drive for the motors
+#define MOTOR_A1_PIN 8// if we do not use DC motor, we can "comment" these lines.
+#define MOTOR_B1_PIN 7//arduino's pin
+int pwmpin = 5;// PWM input 
+int motor = 0;// DC motor initilization
+
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 10;
+
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+unsigned char flagRecv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+int var=0;
+int x=0;
+
+void setup()
+{
+  ax12a.begin(BaudRate, DirectionPin, &Serial);
+    Serial.begin(115200); //can bus connect arduino
+    pinMode(MOTOR_A1_PIN, OUTPUT);// motor shield initilization
+    pinMode(MOTOR_B1_PIN, OUTPUT);//
+  
+    pinMode(pwmpin, OUTPUT);//；
+  // Initialize braked
+
+    digitalWrite(MOTOR_A1_PIN, LOW);//
+    digitalWrite(MOTOR_B1_PIN, LOW);//5（pwm） 
+
+  
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+                                                           // initilization
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+    }
+    Serial.println("CAN BUS Shield init ok!");
+    
+    CAN.init_Filt(0,1,0x10046A01);
+    CAN.init_Mask(0,1,0x1FFFFFFF);
+    CAN.init_Mask(1,1,0x1FFFFFFF); 
+    Serial.println("Mask and filter set!"); 
+    attachInterrupt(0, MCP2515_ISR, FALLING); // start interrupt
+}
+
+void MCP2515_ISR()
+{
+    flagRecv = 1; 
+}
+
+void loop()
+{
+    MCP2515_ISR();
+    if(flagRecv)                   // check if get data
+    {
+        flagRecv = 0;                // clear flag
+//        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        if (x==1) {
+           var=(buf[4]<<8) | buf[3];
+           x=0;
+           if (var==1094) {
+             //Serial.println("D");
+             ax12a.move(ID,512);
+          }
+
+          if (var==1934) {
+              //Serial.println("U");
+              ax12a.move(ID,350);
+          }
+        }
+
+        if(buf[7] >= 0b10000000) {
+          x++;
+        }
+    }
+}
